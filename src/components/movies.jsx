aefa071d826f5376a1f8e5b5474351066e1b5db7
Samit Barai <samitbarai@Samits-MacBook-Pro-2.local>
2019-06-17 17:27:54 +0530

    minor changes

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -8,7 +8,6 @@ import ListGroup from "./common/listGroup";
 import { getGenres } from "../services/fakeGenreService";
 import MoviesTable from "./moviesTable";
 import _ from "lodash";
-import { Link } from "react-router-dom";
 
 class Movies extends Component {
   // State of the component
