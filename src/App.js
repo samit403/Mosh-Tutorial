44d25a05bcf894d2318467863a79abbd01c8bd1c
Samit Barai <samitbarai@Samits-MacBook-Pro-2.local>
2019-06-17 15:46:02 +0530

    Added Navbar and all its plumbing working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
 import NavBar from "./components/navbar";
+import { Route, Switch } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
 
 function App() {
   return (
     <main className="container">
       <NavBar />
-      <Movies />
+      <div className="content">
+        <Switch>
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/" component={Movies} />
+        </Switch>
+      </div>
     </main>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           Vidly
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,19 +22,19 @@ class NavBar extends Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/">
                 Movies <span className="sr-only">(current)</span>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/customers">
                 Customers
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/rentals">
                 Rentals
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
